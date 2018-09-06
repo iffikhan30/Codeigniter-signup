456f72be874e6e4b0c9e1ce90a33a732edf74bca
iffikhan30 <irfan.sarwar.khan30@gmail.com>
2018-09-06 10:40:43 +0500

    Update insert.php

--- models/insert.php
+++ models/insert.php
@@ -5,3 +5,24 @@ public function Insert_record_bytablename($tablename,$data){
 			return false;
 		}
 	}
+public function Insert_Update_options($tablename,$data){
+		$this->db->select('*');
+		$this->db->from($tablename);
+		$this->db->where('metakey', $data['metakey']);
+		$query = $this->db->get();
+		if ($query->num_rows() > 0)
+		{
+			$this->db->where('metakey', $data['metakey']);
+			if($this->db->update($tablename, $data)){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			if($this->db->insert($tablename, $data)){
+				return true;
+			}else{
+				return false;
+			}
+		}
+	}
